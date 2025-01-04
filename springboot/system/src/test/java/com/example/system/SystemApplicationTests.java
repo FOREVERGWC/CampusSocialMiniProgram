0c2cb2c99181089fa73043b2470280e46a50dac8
瑶 <916586595@qq.com>
2025-01-04 14:03:02 +0800

    feat: 初始化后端

--- springboot/system/src/test/java/com/example/system/SystemApplicationTests.java
+++ springboot/system/src/test/java/com/example/system/SystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springboot;
+package com.example.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootApplicationTests {
+class SystemApplicationTests {
 
 	@Test
 	void contextLoads() {
